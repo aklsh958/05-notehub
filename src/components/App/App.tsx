06ecd436e9f99608666f13614bc61fe364534154
Artem Kulish <akulish247@gmail.com>
2025-07-26 12:35:23 +0200

    update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { fetchNotes } from '../../services/noteService';
 import { useDebouncedCallback } from 'use-debounce';
 import NoteList from '../NoteList/NoteList';
-import Paginaition from '../Pagination/Pagination';
+import Pagination from '../Pagination/Pagination'; // Виправив назву імпорту
 import SearchBox from '../SearchBox/SearchBox';
 import Modal from '../Modal/Modal';
 import NoteForm from '../NoteForm/NoteForm';
@@ -14,50 +14,55 @@ import css from '../App/App.module.css';
 export default function App() {
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState('');
-   const [debouncedSearch, setDebouncedSearch] = useState('');
+  const [debouncedSearch, setDebouncedSearch] = useState('');
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  const handleSearch = useDebouncedCallback((search: string) => { setDebouncedSearch(search) }, 300);
+  const handleSearch = useDebouncedCallback((search: string) => {
+    setDebouncedSearch(search);
+  }, 300);
 
-  const handleSearchCange = (search: string) => {
+  const handleSearchChange = (search: string) => {
     setSearch(search);
     setPage(1);
     handleSearch(search);
   };
 
- 
- const { data, isLoading, isError, isSuccess } = useQuery({
+  const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ['notes', page, debouncedSearch],
-    queryFn: () => fetchNotes({
-      page,
-      perPage: 12,
-      search: debouncedSearch,
-    }),
-    placeholderData: keepPreviousData
-  }
-  );
+    queryFn: () =>
+      fetchNotes({
+        page,
+        perPage: 12,
+        search: debouncedSearch,
+      }),
+    placeholderData: keepPreviousData,
+  });
+
+  // Обчислюємо кількість сторінок на основі total кількості нотаток
+  const totalPages = data ? Math.ceil(data.total / 12) : 0;
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox value={search} onChange={handleSearchCange} />
-       {data && data.total_pages > 1 && (
-          <Paginaition
+        <SearchBox value={search} onChange={handleSearchChange} />
+        {totalPages > 1 && (
+          <Pagination
             currentPage={page}
-            totalPages={data.total_pages}
+            totalPages={totalPages}
             onPageChange={setPage}
           />
         )}
-        <button className={css.button} type='button' onClick={() => setModalIsOpen(true)}>Create +</button>
+        <button className={css.button} type="button" onClick={() => setModalIsOpen(true)}>
+          Create +
+        </button>
       </header>
-       {isLoading && <Loader />}
-       {isError && <ErrorMessage />}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
       {isSuccess && data?.data?.length > 0 ? (
-        <NoteList notes={data.data} />)
-        : (
-          <p>No notes found</p>
-        )
-      }
+        <NoteList notes={data.data} />
+      ) : (
+        <p>No notes found</p>
+      )}
       {modalIsOpen && (
         <Modal onClose={() => setModalIsOpen(false)}>
           <NoteForm onClose={() => setModalIsOpen(false)} />
@@ -65,4 +70,4 @@ export default function App() {
       )}
     </div>
   );
-};
+}
